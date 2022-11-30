df5e04d2434faf9a933a299dca8e1c5139806ae4
mahfuz swaron <mahfuzswaron@gmail.com>
2022-11-30 05:55:11 -0500

    hello msgs updated

--- api/Hello.js
+++ api/Hello.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
     try {
-        res.json({ "message": "Hello, sadia's Heart!" });
+        res.json({ "message": "Hello, this is running!" });
     }
     catch (e) {
         res.json(e);
